Fix gradient errors, add debug statements

market_maker/environment.py
@@ -23,11 +23,12 @@ class MarketEnvironment:
 
     def reset(self, initial_theta: np.ndarray) -> torch.Tensor:
         """Reset the environment and optionally return the initial state."""
-        self.current_step: int = 0
+        self.current_step: int = 1
         self.horizon_timestep = 0
         self.model_total_cash: float = 100.0
         self.model_total_inv: int = 10
         self.prices = self.data.select("open, high, low, close").limit(n=1, offset=self.current_step).pl()
+        self.prev_prices = self.data.select("open, high, low, close").limit(n=1, offset=self.current_step - 1).pl()
         self.interest_rates = pl.read_csv("./data/DGS3MO.csv", schema={"observation_date": pl.Datetime, "DGS3MO": pl.Float64}, null_values="").filter(pl.col("observation_date") >= self.start_date)
         # Unfortunately, I only have access to OHLCV data. This could be way more nuanced with L1 or L2 data.
 
@@ -46,8 +47,6 @@ class MarketEnvironment:
         self.prices = self.data.select("open, high, low, close").limit(n=1, offset=self.current_step).pl()
 
         # Model state variables
-        self.model_total_cash: float = 0.0 # profit attributed to change in stock price
-        self.model_total_inv: int = 0
         num_stocks_bought: int = 0 # num stocks bought in the interval
         num_stocks_sold: int = 0 # num stocks sold in the interval
         current_profit: float = 0.0 # the profit recieved from buying/selling in a given time step
@@ -126,7 +125,7 @@ class MarketEnvironment:
         # state = [open, high, low, close, mid_prices_change, total_cash, total_inv, num_stocks_bought, num_stocks_sold, current_profit, current_pnl]
         return self.state, float(reward) # add model attributes: inventory, total cash, last quoted spread, estimated market spread
     
-    def projected_step(self, action: torch.Tensor, simulated_state: torch.Tensor, AIIF: float, horizon_timestep: int) -> tuple[torch.Tensor, float]:
+    def projected_step(self, action: torch.Tensor, simulated_state: torch.Tensor, AIIF: float, horizon_timestep: int, step_forward: bool = False, params: np.ndarray | None = None) -> tuple[torch.Tensor, float, np.ndarray]:
         """Given some action devised by the model at some given state, predict the value of the reward function. Make sure you run `reset_simulation()` first!"""
         # Get parameters (initial parameter is the previous value, current value is the current timestep)
         tau = 1 / 525600
@@ -134,13 +133,18 @@ class MarketEnvironment:
         S = simulated_state[3].item()
         r = self.interest_rates["DGS3MO"][self.simulation_start_index + horizon_timestep]
 
-        # Sample parameters using observations from the previous minute
-        const_params = np.array([S, S0, r, tau])
-        results, accept_rate = hmc_sample_fast(self.current_simulated_theta, const_params, n_samples=10000, step_size=0.0001, n_steps=100)
+        if step_forward: assert horizon_timestep == 0
 
-        # Remove the first 7500 samples as burn-in and compute average params
-        filtered_results = results[7500:, :] # chains tend to converge after ~7500 iterations of burn-in
-        kappa, theta, sigma, rho, v0 = np.mean(filtered_results, axis=0)
+        if params is None:
+            # Sample parameters using observations from the previous minute
+            const_params = np.array([S, S0, r, tau])
+            results, accept_rate = hmc_sample_fast(self.current_simulated_theta, const_params, n_samples=10000, step_size=0.0001, n_steps=100)
+
+            # Remove the first 7500 samples as burn-in and compute average params
+            filtered_results = results[7500:, :] # chains tend to converge after ~7500 iterations of burn-in
+            kappa, theta, sigma, rho, v0 = np.mean(filtered_results, axis=0)
+        else:
+            kappa, theta, sigma, rho, v0 = params
         
         # Get expected path over the next minute
         expected_path = np.mean(generate_sample_paths(tau, kappa, theta, sigma, rho, v0, S, r, 1000, 100)[0], axis=0)
@@ -158,12 +162,16 @@ class MarketEnvironment:
         DITF = model_total_cash / (model_total_inv * close_price)
         inv_penalty = AIIF * min(1, float(np.mean(self.simulated_inv_count) / np.mean(self.simulated_dynamic_thresholds)))
 
-        # Appends
-        self.simulated_mid_prices.append(mid_price)
-        self.simulated_profit.append(current_profit)
-        self.simulated_inv_count.append(model_total_inv)
-        self.simulated_dynamic_thresholds.append(DITF * abs(model_total_cash / np.mean(self.simulated_mid_prices)))
-        self.simulated_pnl.append(model_total_inv * (close_price - self.simulated_prev_close)) # evaluate price change effect on current inventory (pnl)
+        if step_forward:
+            # Appends
+            self.simulated_mid_prices.append(mid_price)
+            self.simulated_profit.append(current_profit)
+            self.simulated_inv_count.append(model_total_inv)
+            self.simulated_dynamic_thresholds.append(DITF * abs(model_total_cash / np.mean(self.simulated_mid_prices)))
+            self.simulated_pnl.append(model_total_inv * (close_price - self.simulated_prev_close)) # evaluate price change effect on current inventory (pnl)
+            
+            # Step forward
+            self.horizon_timestep += 1
 
         # Evaluate reward given action
         reward = current_profit + self.simulated_pnl[-1] - hedging_penalty - inv_penalty
@@ -180,18 +188,20 @@ class MarketEnvironment:
         model_state = torch.tensor([model_total_cash, model_total_inv, num_stocks_bought, num_stocks_sold, current_profit, current_pnl])
         new_simulated_state = torch.hstack([env_state, model_state])
 
-        self.horizon_timestep += 1
-
-        return new_simulated_state, reward.item() # add model attributes: inventory, total cash, last quoted spread, estimated market spread
+        return new_simulated_state, reward, np.array([kappa, theta, sigma, rho, v0]) # add model attributes: inventory, total cash, last quoted spread, estimated market spread
     
     @staticmethod
     def _order_fill(action: torch.Tensor, state: torch.Tensor) -> tuple[float, float, float, float, float, int]:
         # Action
         buy_limit_price, buy_limit_num, sell_limit_price, sell_limit_num, num_stocks_market = action.tolist()
+        buy_limit_num = int(buy_limit_num)
+        sell_limit_num = int(sell_limit_num)
+        num_stocks_market = int(num_stocks_market)
+        # num_stocks_market = 0
 
         # State
         open_price, high_price, low_price, close_price = state.tolist()[:4]
-        mid_prices_change, model_total_cash, model_total_inv, num_stocks_bought, num_stocks_sold, current_profit = state.tolist()[5:]
+        mid_prices_change, model_total_cash, model_total_inv, num_stocks_bought, num_stocks_sold, current_profit, current_pnl = state.tolist()[4:]
         spread_estimate = high_price - low_price
         mid_price = (high_price + low_price) / 2
         fee_per_stock = 0.005
@@ -211,6 +221,7 @@ class MarketEnvironment:
             current_profit -= buy_cost
             model_total_inv += int(buy_limit_num)
             num_stocks_bought += int(buy_limit_num)
+            print(f"BUY LIMIT SATISFIED: Bought {num_stocks_bought} at {buy_cost / buy_limit_num} per share.")
         elif sell_limit_num > 0 and high_price >= sell_limit_price and model_total_inv - sell_limit_num >= 0:
             # Sell order (trade executes at a price in [next_price, high_price])
             sell_price = (np.random.uniform(close_price, high_price) - fee_per_stock) * sell_limit_num
@@ -218,6 +229,7 @@ class MarketEnvironment:
             current_profit += sell_price
             model_total_inv -= int(sell_limit_num)
             num_stocks_sold += int(sell_limit_num)
+            print(f"SELL LIMIT SATISFIED: Sold {abs(num_stocks_sold)} at {sell_price / sell_limit_num} per share.")
         
         # Market orders -- model pays the bid-ask spread
         market_price = np.random.uniform(mid_price - spread_estimate / 2, mid_price + spread_estimate / 2)
@@ -228,6 +240,7 @@ class MarketEnvironment:
             current_profit -= market_price + hedging_penalty
             model_total_inv += int(num_stocks_market)
             num_stocks_bought += int(num_stocks_market)
+            print(f"BUY MARKET SATISFIED: Bought {num_stocks_market} at {market_price}.")
         elif num_stocks_market < 0 and model_total_inv - abs(num_stocks_market) >= 0:
             # Sell order
             hedging_penalty = spread_estimate * abs(num_stocks_market)
@@ -235,7 +248,9 @@ class MarketEnvironment:
             current_profit += market_price - hedging_penalty
             model_total_inv -= int(abs(num_stocks_market))
             num_stocks_sold += int(abs(num_stocks_market))
+            print(f"SELL MARKET SATISFIED: Sold {abs(num_stocks_market)} at {market_price}.")
         
+        print(f"Summary:\nStocks bought: {num_stocks_bought}\nStocks sold: {num_stocks_sold}\nCurrent profit: {current_profit}\nTotal cash: {model_total_cash}\nTotal inv: {model_total_inv}\nEstimated total value: {model_total_cash + model_total_inv * market_price}")
         return num_stocks_bought, num_stocks_sold, current_profit, hedging_penalty, model_total_cash, model_total_inv
 
 

market_maker/model.py
@@ -8,6 +8,9 @@ from typing import Literal
 
 
 AIIF = 0.5
+INV_LIMIT = 10
+PRICE_LIMIT = 0.005
+# torch.autograd.set_detect_anomaly(True)
 
 
 class ReplayBuffer:
@@ -51,28 +54,38 @@ def mlp(sizes: list[int], activation, output_activation) -> nn.Sequential:
 
 # Gaussian Policy network for action selection
 class GaussianPolicy(nn.Module):
-    def __init__(self, obs_dim: int, act_dim: int, hidden: tuple[int, int] = (256,256), log_std_min: float = -20, log_std_max: float = 2):
+    def __init__(self, obs_dim: int, act_dim: int, hidden: tuple[int, int] = (256, 256), log_std_min: float = -20, log_std_max: float = 2):
         super().__init__()
         self.net = mlp([obs_dim]+list(hidden), activation=nn.ReLU, output_activation=nn.ReLU)
         self.mu = nn.Linear(hidden[-1], act_dim)
         self.log_std = nn.Linear(hidden[-1], act_dim)
         self.log_std_min, self.log_std_max = log_std_min, log_std_max
+        self.lower_bounds = torch.tensor([-PRICE_LIMIT, 0, 0, 0, -INV_LIMIT])
+        self.upper_bounds = torch.tensor([0, INV_LIMIT, PRICE_LIMIT, INV_LIMIT, INV_LIMIT])
 
     def forward(self, obs: torch.Tensor) -> tuple[torch.Tensor, float, float]:
         x = self.net(obs)
         mu = self.mu(x)
         log_std = torch.clamp(self.log_std(x), self.log_std_min, self.log_std_max)
         std = torch.exp(log_std)
-        # reparameterize
+
+        # Reparameterize
         eps = torch.randn_like(mu)
         pre_tanh = mu + eps * std
         action = torch.tanh(pre_tanh)  # squashed to (-1, 1)
-        # log_prob with tanh correction:
+
+        # Log_prob with tanh correction:
         log_prob = -0.5 * ((eps)**2 + 2*log_std + np.log(2*np.pi))
         log_prob = log_prob.sum(dim=-1, keepdim=True)
-        # correction for tanh
+
+        # Correction for tanh
         log_prob -= torch.log(1 - action.pow(2) + 1e-6).sum(dim=-1, keepdim=True)
-        return action, log_prob, mu
+
+        # Rescale action
+        action_rescaled = self.lower_bounds + (0.5 * (action + 1.0)) * (self.upper_bounds - self.lower_bounds)
+        close_price_modification = torch.tensor([obs[0][3], 0, obs[0][3], 0, 0])
+
+        return action_rescaled + close_price_modification, log_prob, mu
 
 # Q Networks for the actors and critics
 class QNetwork(nn.Module):
@@ -85,7 +98,7 @@ class QNetwork(nn.Module):
 
 # SAC Agent
 class SACAgent:
-    def __init__(self, state_dim: int = 12, action_dim: int = 5, device: Literal["cpu", "cuda"] = "cpu"):
+    def __init__(self, state_dim: int = 11, action_dim: int = 5, device: Literal["cpu", "cuda"] = "cpu"):
         self.device = device
         self.actor = GaussianPolicy(state_dim, action_dim).to(device)
         self.q1 = QNetwork(state_dim, action_dim).to(device) # min of these two networks is used to stabilize learning
@@ -103,7 +116,7 @@ class SACAgent:
 
         self.alpha = 0.2  # entropy regularization parameter
         self.gamma = 0.99 # discount factor
-        self.rho = 0.995 # polyak; helps smooth target updates to stabilize learning; -> 1 means slower updates
+        self.rho = 0.995  # polyak; helps smooth target updates to stabilize learning; -> 1 means slower updates
 
     def select_action(self, state: torch.Tensor, deterministic: bool = False): # deterministic at testing time; uses mean
         s_t = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
@@ -120,16 +133,12 @@ class SACAgent:
         reward = batch['reward'].float().to(self.device)
         next_state = batch['next_state'].float().to(self.device)
 
-        # Actor Q Function
-        a_pi, logp_pi, _ = self.actor(current_state) # pick action
-        q1_pi = self.q1(current_state, a_pi) # grade action
-        q2_pi = self.q2(current_state, a_pi)
-        q_pi = torch.min(q1_pi, q2_pi)
-
         # Critic Q Function
         with torch.no_grad():
             # Generate an action for the next state
             next_action, logp_next_action, _ = self.actor(next_state)
+            next_action = next_action.detach()
+            logp_next_action = logp_next_action.detach()
 
             # Evaluate Q function of next_action in next_state
             q1_t = self.q1_target(next_state, next_action)
@@ -148,32 +157,46 @@ class SACAgent:
         self.q1_opt.zero_grad(); q1_loss.backward(); self.q1_opt.step()
         self.q2_opt.zero_grad(); q2_loss.backward(); self.q2_opt.step()
 
+        # Actor Q Function
+        a_pi, logp_pi, _ = self.actor(current_state) # pick action
+        q1_pi = self.q1(current_state, a_pi) # grade action
+        q2_pi = self.q2(current_state, a_pi)
+        q_pi = torch.min(q1_pi, q2_pi)
+
         # Actor Update
-        actor_loss: torch.Tensor = (self.alpha * logp_pi - q_pi).mean()
+        actor_loss = (self.alpha * logp_pi - q_pi).mean()
         self.actor_opt.zero_grad(); actor_loss.backward(); self.actor_opt.step()
 
         # Soft updates: theta_targ * rho + (1 - rho) * theta
-        for p, tp in zip(self.q1.parameters(), self.q1_target.parameters()):
-            tp.data.mul_(self.rho)
-            tp.data.add_((1 - self.rho) * p.data)
-        for p, tp in zip(self.q2.parameters(), self.q2_target.parameters()):
-            tp.data.mul_(self.rho)
-            tp.data.add_((1 - self.rho) * p.data)
+        with torch.no_grad():
+            for p, tp in zip(self.q1.parameters(), self.q1_target.parameters()):
+                tp.data.mul_(self.rho)
+                tp.data.add_((1 - self.rho) * p.data)
+            for p, tp in zip(self.q2.parameters(), self.q2_target.parameters()):
+                tp.data.mul_(self.rho)
+                tp.data.add_((1 - self.rho) * p.data)
 
 # Transition simulation
-def generate_simulated_transitions(env: MarketEnvironment, state: torch.Tensor, action: torch.Tensor, n_samples: int = 5):
+def generate_simulated_transitions(env: MarketEnvironment, initial_state: torch.Tensor, initial_action: torch.Tensor, n_samples: int = 1):
     """
     env: environment providing reward function `env.reward(s,a,s_next)` and done predicate or terminal check
     stochastic_model.sample_next_states(s, a, n_samples) -> np.array (n_samples, state_dim)
     Returns list of (s, a, r, s_next)
     """
-    # s_next_samples = stochastic_model.sample_next_states(s, action, n_samples)  # (n_samples, state_dim)
-    simulated_next_state, r = env.projected_step(action, state, AIIF, horizon_timestep=0)
-    return [(state, action, r, simulated_next_state)]
+    current_state = initial_state
+    current_action = initial_action
+    params = None
+    transitions = []
+    for i in range(n_samples):
+        print(f"Simulation #{i}...")
+        simulated_next_state, reward, params = env.projected_step(current_action, current_state, AIIF, horizon_timestep=0, step_forward=False, params=params)
+        transitions.append((current_state, current_action, reward, simulated_next_state))
+    
+    return transitions
 
 # Training loop
-def train_loop(env: MarketEnvironment, initial_theta: np.ndarray, num_steps: int = 100000):
-    state_dim = 12
+def train_loop(env: MarketEnvironment, initial_theta: np.ndarray, num_steps: int = 100000, plan: bool = True):
+    state_dim = 11
     action_dim = 5
     buffer = ReplayBuffer(200000, state_dim, action_dim)
     agent = SACAgent(state_dim, action_dim)
@@ -183,16 +206,36 @@ def train_loop(env: MarketEnvironment, initial_theta: np.ndarray, num_steps: int
         action = agent.select_action(initial_state)
         next_state, reward = env.step(action, AIIF)
         buffer.add(initial_state, action, reward, next_state) # add transition to replay buffer
+        print(f"Initial state: {initial_state}")
+        print(f"Action: {action}")
+        print(f"Final state: {next_state}")
+        print(f"Reward: {reward}")
 
         # Dyna imagination: generate model-based transitions from the *real* (s,a)
-        imagined = generate_simulated_transitions(env, initial_state, action, n_samples=8)
-        for (s_i, a, r, s_f) in imagined:
-            buffer.add(s_i, a, r, s_f)
+        if plan:
+            imagined = generate_simulated_transitions(env, initial_state, action, n_samples=10)
+            for (s_i, a, r, s_f) in imagined:
+                buffer.add(s_i, a, r, s_f)
 
         # Train
-        if buffer.size > 1024:
+        if buffer.size > 1024: # 1024
+            print("Training now...")
             for _ in range(1):  # gradient steps per env step
                 batch = buffer.sample(256)
                 agent.update(batch)
+        
+        initial_state = next_state
 
     return agent
+
+if __name__ == "__main__":
+    kappa = 2.0
+    theta = 0.04
+    sigma = 0.3
+    rho = -0.7
+    v0 = 0.04
+    initial_theta = np.array([kappa, theta, sigma, rho, v0])
+    env = MarketEnvironment("AAPL", r"C:\Users\tfgor\Documents\BayesianMarketMaker\data\deltalake", "2010-01-01", initial_theta)
+    print("Done")
+
+    train_loop(env, initial_theta, plan=False)